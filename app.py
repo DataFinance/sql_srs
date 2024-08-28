4eb3de5e609b2c6631859031a406d18f7cfd195a
noel <noelraba@gmail.com>
2024-08-28 03:34:33 +0200

    add ci version3 après changement mention de version ubuntu et maintenant push de la version clean de la app

--- app.py
+++ app.py
@@ -1,43 +1,71 @@
-import pandas as pd
-import duckdb
-import streamlit as st
+# pylint: disable=missing-module-docstring
 import io
 
-from pandas.core.computation.common import result_type_many
+import duckdb
+import pandas as pd
+import streamlit as st
 
-csv='''
+CSV = """
 beverage,price
 orange juice,2.5
 espresso,2
 tea,3
-'''
-beverages=pd.read_csv(io.StringIO(csv))
+"""
+beverages = pd.read_csv(io.StringIO(CSV))
 
-csv2='''
+CSV2 = """
 food_item,food_price
 cookie juice,2.5
 chocolatine,2
 muffin,3
-'''
+"""
 
-food_items=pd.read_csv(io.StringIO(csv2))
+food_items = pd.read_csv(io.StringIO(CSV2))
 
-answer="""
+ANSWER_STR = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
 
-solution=duckdb.sql(answer).df()
+solution_df = duckdb.sql(ANSWER_STR).df()
 
-st.header("enter your code:")
-query = st.text_area(label="Votre code SQL ici", key="user_input")
+st.header("Enter your code please : ")
+query = st.text_area(label="Votre code SQL ici svp", key="user_input")
+
 
 if query:
+
     result = duckdb.sql(query).df()
     st.dataframe(result)
 
+    try:
+        result = result[solution_df.columns]
+        # result=result[["beverage","price","food_item","food_price"]]
+        st.dataframe(result.compare(solution_df))
 
-tab2,tab3=st.tabs(["Tables","Solution"])
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+
+    if n_lines_difference != 0:
+        st.write(
+            f"result had a {n_lines_difference} lines difference with the solution"
+        )
+
+
+with st.sidebar:
+    option = st.selectbox(
+        "What would you like to review ?",
+        ("Joins", "Group By", "Window Functions"),
+        index=None,
+        placeholder="Select a them to practice...",
+    )
+
+    st.write("You selected :", option)
+
+
+tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
     st.write("Tables: beverages")
@@ -45,19 +73,7 @@ with tab2:
     st.write("Tables: food_items")
     st.dataframe(food_items)
     st.write("expected")
-    st.dataframe(solution)
+    st.dataframe(solution_df)
 
 with tab3:
-    st.write(answer)
-
-
-
-
-
-
-
-
-
-
-
-
+    st.write(ANSWER_STR)
